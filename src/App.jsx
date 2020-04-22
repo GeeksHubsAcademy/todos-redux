02ca7b923677bfc7a3eb1d04876417ae42930d1b
jupegarnica <jupegarnica@gmail.com>
2020-04-22 18:29:33 +0200

    redux installed

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+function App(props) {
+  return (
+    <div className="App">
+          <pre>
+            {
+              JSON.stringify(props, null, 2)
+            }
+          </pre>
+          <button onClick={() => props.addRandomTodo()}>
+              add todo
+          </button>
+
+    </div>
+  );
+}
+
+const mapStateToProps = state => ({state: state})
+const mapDispatchToProps = (dispatch) => ({
+  addRandomTodo: () =>
+    dispatch({
+      type: 'ADD_TODO',
+      payload: 'borra esta tarea!',
+      completed: Math.random() > 0.5 ? true : false
+    }),
+});
+const connectedApp = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  )
+  (App);
+
+export default connectedApp;
---
+++ src/services/redux/reducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+    visibility: 'ALL',
+    todos:[]
+}
+
+
+function reducer(state = initialState, action) {
+    console.log(action);
+
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                 ...state,
+                 todos: [
+                     ...state.todos,
+                     {
+                         text:action.payload,
+                         completed: action.completed || false,
+                     }
+                 ]
+            };
+
+        default:
+             return state;
+    }
+
+
+}
+
+
+export default reducer;
